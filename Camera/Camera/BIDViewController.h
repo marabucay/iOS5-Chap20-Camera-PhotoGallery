f9b116379e77a68824811d2b38a4f946e3b60da8
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-25 10:42:49 +0800

    Declaring Outlet, Actions and Interfaces.

--- Camera/Camera/BIDViewController.h
+++ Camera/Camera/BIDViewController.h
@@ -7,7 +7,17 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MediaPlayer/MediaPlayer.h>
 
-@interface BIDViewController : UIViewController
+@interface BIDViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIButton *takePictureButton;
+@property (strong, nonatomic) MPMoviePlayerController *moviePlayerController;
+@property (strong, nonatomic) UIImage *image;
+@property (strong, nonatomic) NSURL *movieURL;
+@property (copy, nonatomic) NSString *lastChosenMediaType;
+@property (assign, nonatomic) CGRect imageFrame;
+- (IBAction)shootPictureOrVideo:(id)sender;
+- (IBAction)selectExistingPictureOrVideo:(id)sender;
 
 @end
